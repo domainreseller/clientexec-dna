3a156e0e29348a24d42a02c58ec76b71938b2455
xa81 <poisonkid@gmail.com>
2026-03-02 02:58:49 +0300

    Bump version to 1.0.15

--- PluginDomainnameapi.php
+++ PluginDomainnameapi.php
@@ -6,7 +6,7 @@ require_once 'plugins/registrars/domainnameapi/api.php';
 
 class PluginDomainnameapi extends RegistrarPlugin
 {
-    public const MODULE_VERSION='1.0.14';
+    public const MODULE_VERSION='1.0.15';
     public $features = [
         'nameSuggest' => false,
         'importDomains' => true,
